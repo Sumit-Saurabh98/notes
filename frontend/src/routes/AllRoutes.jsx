d65863db5de4a63709250fb0340c62b0addb908e
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-09-30 22:38:13 +0530

    done

--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -5,11 +5,13 @@ import Signup from '../pages/Signup';
 import Notes from '../pages/Notes';
 import SingleNotes from '../pages/SingleNotes';
 import PrivateRoute from './PrivateRoute';
+import HomePage from '../pages/Homepage';
 
 function AllRoutes(props) {
     return (
         <Routes>
-            <Route path='/' element={<Login/>}/>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/login' element={<Login/>}/>
             <Route path='/signup' element={<Signup/>}/>
             <Route path='/notes' element={<PrivateRoute><Notes/></PrivateRoute>}/>
             <Route path='/notes/:id' element={<PrivateRoute><SingleNotes/></PrivateRoute>}/>
