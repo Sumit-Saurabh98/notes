9d5d8999d981bf3282118fce781124c4d096b8b4
sumit-saurabh98 <sumitsaurabh112@gamil.com>
2025-04-04 17:29:05 +0530

    prod-2

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const taskapi = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002" : "",
+    baseURL: import.meta.env.MODE === "development" ? "https://notes-production-a37c.up.railway.app" : "",
     withCredentials: true, // Important for sending/receiving cookies
 });
 
